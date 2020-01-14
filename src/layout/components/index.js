893d4ae893c7a8e5a4baee4304487454d5b8279a
xlong1029 <381612175@qq.com>
2020-01-14 14:31:04 +0800

    测试发布

--- src/layout/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AppMain } from './AppMain'
-export { default as Navbar } from './Navbar'
-export { default as AppFooter } from './AppFooter'
-export { default as Breadcrumb } from './Breadcrumb'
--- src/utils/get-page-title.js
+++
@@ -1,8 +0,0 @@
-const title = 'XLONG家里蹲管理系统'
-
-export default function getPageTitle(pageTitle) {
-  if (pageTitle) {
-    return `${pageTitle} - ${title}`
-  }
-  return `${title}`
-}
